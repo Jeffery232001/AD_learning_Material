8156d4c2c2a48c7a567fb36c27fd50b70aa49a4c
JEFFERY ESSIEN <Jeffssien23@gmail.com>
2026-09-14 07:30:23 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Active from './components/Active';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Active Directory clone Asset App</h1>
+      <Active />
+    </div>
+  );
+}
+
+export default App;
